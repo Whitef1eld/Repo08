cc281acf2352c4ba6fd007aa9520e720fa9e4342
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-21 01:04:43 +0200

    Functions - Returning boolean values

---
+++ ex09/src/index.js
@@ -0,0 +1,18 @@
+// only change code below this line
+function isLess(a, b) {
+    // only change code above this line
+    if(a < b) {
+        // only change code below this line
+        return true;
+        // only change code above this line
+    } else {
+        // only change code below this line
+        return false;
+        // only change code above this line
+    }
+    // only change code below this line
+}
+console.log(isLess(10, 15));
+console.log(isLess(15, 10));
+// only change code above this line
+module.exports = isLess;
