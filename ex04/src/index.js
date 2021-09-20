1f82deadc67cf08954989c08f725fa58ad54ad12
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-21 00:32:05 +0200

    Added the function - local scope

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -8,9 +8,9 @@ localScope();
 
 // myVariable is not defined outside of localScope
 if(typeof myVariable != "undefined") {
-    console.log('outside localScope', myVariable);
+    console.log('outside localScope', myVariable)
 } else {
-    console.log('outside localScope UNDEFINED!!!');
+    console.log('outside localScope UNDEFINED!!!')
 }
 
 module.exports = localScope;
