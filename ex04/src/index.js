a8f4ba4e24e76793253346cec3673ab2625834a7
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-20 15:20:33 +0200

    Adding the functions output - localScope

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+// only change code below this line
+function localScope() {
+    var myVariable = "I am here!";
+    console.log('inside localScope', myVariable);
+}
+localScope();
+// only change code above this line
+
+// myVariable is not defined outside of localScope
+if(typeof myVariable != "undefined") {
+    console.log('outside localScope', myVariable);
+} else {
+    console.log('outside localScope UNDEFINED!!!');
+}
+
+module.exports = localScope;
