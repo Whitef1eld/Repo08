50946fbab8edcbe1af89e069651c8798a4027744
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-20 16:02:23 +0200

    Adding a function which is returning a value

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+// only change code below this line
+function xSix(num) {
+    return 6*num;
+}
+
+console.log(xSix(5)); // change this line
+console.log(xSix(2)); // change this line
+console.log(xSix(0)); // change this line
+// only change code above this line
+
+module.exports = xSix;
