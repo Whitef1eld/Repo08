b03985bd0d84da29927c02ac858104cb10129d4c
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-21 01:53:28 +0200

    generating a random number

---
+++ ex12/src/index.js
@@ -0,0 +1,8 @@
+// only change code below this line
+function randomInteger() {
+    return Math.floor(Math.random() * 30);
+}
+
+console.log(randomInteger());
+// only change code above this line
+module.exports = randomInteger;
