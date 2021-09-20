e546d8247ff1bac7867c5246caa04a337734deef
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-21 00:49:37 +0200

    Functions - understanding undefined value

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -3,7 +3,16 @@ var sum = 0;
 
 function addThree() {
     sum += 3;
+    console.log('addThree:', sum);
 }
+
+function addFive() {
+    sum += 5;
+    console.log('addFive:', sum);
+}
+
+addThree();
+addFive();
 // only change code above this line
 module.exports = {
     addThree,
