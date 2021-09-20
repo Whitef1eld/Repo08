a7cd83719632892c4ec0272064d7af393fd79eb1
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-21 00:46:21 +0200

    Added the files for ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+// only change code below this line
+var sum = 0;
+
+function addThree() {
+    sum += 3;
+}
+// only change code above this line
+module.exports = {
+    addThree,
+    addFive
+};
