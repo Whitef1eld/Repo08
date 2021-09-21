1cc6bac53814466e93cb21f022b06439f0075310
Whitef1eld <nwhitefiel1@etf.unsa.ba>
2021-09-21 02:02:09 +0200

    returning the parseInt - string to integers

---
+++ ex14/src/index.js
@@ -0,0 +1,10 @@
+// only change code below this line
+function convertStrToInt(str) {
+    return parseInt(str);
+}
+
+console.log(convertStrToInt("44"));
+console.log(convertStrToInt("27"));
+console.log(convertStrToInt("JeffBezos"));
+// only change code above this line
+module.exports = convertStrToInt;
